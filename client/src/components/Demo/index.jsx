18766ca27524bd55df8c5ac2466ba72a013e77f9
Karol Kasprzak <karol.kasprzak@boldare.com>
2025-01-03 11:35:07 +0100

    Displa ChainList smart contract fields

--- client/src/components/Demo/index.jsx
+++ client/src/components/Demo/index.jsx
@@ -10,7 +10,12 @@ import NoticeWrongNetwork from "./NoticeWrongNetwork";
 
 function Demo() {
   const { state } = useEth();
-  const [value, setValue] = useState("?");
+  const [value, setValue] = useState({
+    "seller": "?",
+    "name": "?",
+    "description": "?",
+    "price": "?"
+  });
 
   const demo =
     <>
